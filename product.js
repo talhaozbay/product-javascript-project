147b11e86d273c8e7556ecce269c4389040a3c7d
talhaozbay <oyun_kolu_44@hotmail.com>
2021-08-17 15:43:28 +0300

    cons resolved

--- product.js
+++ product.js
@@ -1,19 +1,8 @@
-const {Client} = require('pg')
-
-var client = new Client({
-    host: "localhost",
-    user: "postgres",
-    port: 5432,
-    password: "1234",
-    database: "product"
-})
-
-client.connect();
-
-client.query(`Select * from users` , (err, res) =>{
-    if(!err){
-        console.log(res.rows);
-    }else{
-        console.log(err.message)
+$.ajax({
+    type: "GET",
+    url: "http://localhost:8080/product",
+    dataType: "json",
+    success: function (response) {
+        console.log("success!!!")
     }
-})
+});
