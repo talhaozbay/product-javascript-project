2782301f783c8641aa523232a8ca2ef8d2d1e8be
talhaozbay <oyun_kolu_44@hotmail.com>
2021-08-19 09:48:04 +0300

    add getproduct

--- product.js
+++ product.js
@@ -1,18 +1,25 @@
-$.ajax({
-    type: "GET",
-    url: "http://localhost:8080/product",
-    dataType: "json",
-    success: function (response) {
-        console.log("success!!!!!")
-    }
-});
-$.getJSON("http://localhost:8080/product", function (data) {
-    var items = [];
-    $.each(data, function (key, val) {
-        items.push("<td>" + val + "</td>");
-    });
+let productList=[];
 
-    $("<tr/>", {
-        html: items.join("")
-    }).appendTo("tbody");
-});
+function getProduct() {
+$.getJSON("http://localhost:8080/product", function (data) {
+    
+    productList = data;
+    console.log(typeof(productList));
+    var items = "";
+    console.log(data)
+    $.each(data, function (key, val) {
+        items += "<tr>";
+        items += "<td>" + val.name + "</td>";
+        items += "<td>" + val.unitprice + "</td>";
+        items += "<td>" + val.code + "</td>";
+        items += "<td>" + val.active + "</td>";
+        items += "</tr>";
+
+    });
+    $("#productTable").html(items);
+})}
+
+
+getProduct();
+
+// . class // # id
